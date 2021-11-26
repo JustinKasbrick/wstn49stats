7803dc7bbda9da6d9cddb60bc70ef045fd2456ab
JustinKasbrick <justink@iqmetrix.com>
2021-11-26 13:44:31 -0600

    Added some test data

---
+++ wstn49stats/PickGames/Pick2Import.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wstn49stats.PickGames
+{
+    public class Pick2Import
+    {
+        public void Import()
+        {
+
+        }
+    }
+}
